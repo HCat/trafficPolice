2243d35e1ab017d0c3f0030cba44c8ee4fbaecb4
hcat-89 <375348234@qq.com>
2017-05-17 17:15:26 +0800

    修复倒计时问题，完成个人登录页面和逻辑，优化MBProgressHUD工具类的显示问题, 在ShareFun中，增加手机验证码格式，高亮显示NSString中数据问题。增加模块alertView.

---
+++ trafficPolice/Views/AlertView.h
@@ -0,0 +1,13 @@
+//
+//  AlertView.h
+//  trafficPolice
+//
+//  Created by hcat-89 on 2017/5/17.
+//  Copyright © 2017年 Degal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlertView : UIView
+
+@end
---
+++ trafficPolice/Views/AlertView.m
@@ -0,0 +1,21 @@
+//
+//  AlertView.m
+//  trafficPolice
+//
+//  Created by hcat-89 on 2017/5/17.
+//  Copyright © 2017年 Degal. All rights reserved.
+//
+
+#import "AlertView.h"
+
+@implementation AlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
