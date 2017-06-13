4fc92076865edc0fb0b8ec2e870e497f6d8ca7d3
hcat <hcat@MACdeMac-mini.local>
2017-06-13 16:34:31 +0800

    优化拍照摸层的拍照框

--- trafficPolice/Controllers/MainHomeVC/LRCameraVC/maskingView.m
+++ trafficPolice/Controllers/MainHomeVC/LRCameraVC/maskingView.m
@@ -8,11 +8,26 @@
 
 #import "maskingView.h"
 
+#define LineLength 20
+#define LineWeight 2
+
 @interface maskingView()
 
 @property (nonatomic,strong) CAShapeLayer * shapeLayer;
 @property (nonatomic,strong)  UIBezierPath *circlePath;
 @property (nonatomic,assign) BOOL isClearnBorder;
+
+@property (nonatomic,strong) UIBezierPath *firstSidePath;
+@property (nonatomic,strong) CAShapeLayer * firstSideLayer;
+
+@property (nonatomic,strong) UIBezierPath *secondSidePath;
+@property (nonatomic,strong) CAShapeLayer * secondSideLayer;
+
+@property (nonatomic,strong) UIBezierPath *thirdSidePath;
+@property (nonatomic,strong) CAShapeLayer * thirdSideLayer;
+
+@property (nonatomic,strong) UIBezierPath *fourthSidePath;
+@property (nonatomic,strong) CAShapeLayer * fourthSideLayer;
 
 @end
 
@@ -31,6 +46,19 @@
         self.isClearnBorder = NO;
         self.shapeLayer = [CAShapeLayer layer];
         [self.layer addSublayer:self.shapeLayer];
+        
+        self.firstSideLayer = [CAShapeLayer layer];
+        [self.layer addSublayer:self.firstSideLayer];
+        
+        self.secondSideLayer = [CAShapeLayer layer];
+        [self.layer addSublayer:self.secondSideLayer];
+        
+        self.thirdSideLayer = [CAShapeLayer layer];
+        [self.layer addSublayer:self.thirdSideLayer];
+        
+        self.fourthSideLayer = [CAShapeLayer layer];
+        [self.layer addSublayer:self.fourthSideLayer];
+        
     
     }
     return self;
@@ -55,10 +83,52 @@
         [self setNeedsDisplay];
         self.isClearnBorder = YES;
         
+        
+        
         self.circlePath = [UIBezierPath bezierPathWithRect:myRect];
        
         [path appendPath:_circlePath];
         [path setUsesEvenOddFillRule:YES];
+        
+        //左上角
+        self.firstSidePath = [UIBezierPath bezierPath];
+        [self.firstSidePath moveToPoint:CGPointMake(CGRectGetMinX(myRect)+LineLength, CGRectGetMinY(myRect))];
+        [self.firstSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMinY(myRect))];
+        [self.firstSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMinY(myRect)+LineLength)];
+        self.firstSideLayer.path = self.firstSidePath.CGPath;
+        self.firstSideLayer.lineWidth = LineWeight;
+        self.firstSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        
+        //左下角
+        self.secondSidePath = [UIBezierPath bezierPath];
+        [self.secondSidePath moveToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMaxY(myRect)-LineLength)];
+        [self.secondSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMaxY(myRect))];
+        [self.secondSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect)+LineLength, CGRectGetMaxY(myRect))];
+        self.secondSideLayer.path = self.secondSidePath.CGPath;
+        self.secondSideLayer.lineWidth = LineWeight;
+        self.secondSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        //右下角
+        self.thirdSidePath = [UIBezierPath bezierPath];
+        [self.thirdSidePath moveToPoint:CGPointMake(CGRectGetMaxX(myRect)-LineLength, CGRectGetMaxY(myRect))];
+        [self.thirdSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMaxY(myRect))];
+        [self.thirdSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMaxY(myRect)-LineLength)];
+        self.thirdSideLayer.path = self.thirdSidePath.CGPath;
+        self.thirdSideLayer.lineWidth = LineWeight;
+        self.thirdSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        //右上角
+        self.fourthSidePath = [UIBezierPath bezierPath];
+        [self.fourthSidePath moveToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMinY(myRect)+LineLength)];
+        [self.fourthSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMinY(myRect))];
+        [self.fourthSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect)-LineLength, CGRectGetMinY(myRect))];
+        self.fourthSideLayer.path = self.fourthSidePath.CGPath;
+        self.fourthSideLayer.lineWidth = LineWeight;
+        self.fourthSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+
+        _firstSideLayer.fillColor = [UIColor clearColor].CGColor;
+        _secondSideLayer.fillColor = [UIColor clearColor].CGColor;
+        _thirdSideLayer.fillColor = [UIColor clearColor].CGColor;
+        _fourthSideLayer.fillColor = [UIColor clearColor].CGColor;
+
         
         self.shapeLayer.path = path.CGPath;
         self.shapeLayer.fillRule = kCAFillRuleEvenOdd;
@@ -93,9 +163,52 @@
     [self setNeedsDisplay];
     self.isClearnBorder = YES;
     
+    
     self.circlePath = [UIBezierPath bezierPathWithRect:myRect];
     [path appendPath:_circlePath];
     [path setUsesEvenOddFillRule:YES];
+    
+    //左上角
+    self.firstSidePath = [UIBezierPath bezierPath];
+    [self.firstSidePath moveToPoint:CGPointMake(CGRectGetMinX(myRect)+ LineLength, CGRectGetMinY(myRect))];
+    [self.firstSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMinY(myRect))];
+    [self.firstSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMinY(myRect)+LineLength)];
+    self.firstSideLayer.path = self.firstSidePath.CGPath;
+    self.firstSideLayer.lineWidth = LineWeight;
+    self.firstSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+    
+    //左下角
+    self.secondSidePath = [UIBezierPath bezierPath];
+    [self.secondSidePath moveToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMaxY(myRect)-LineLength)];
+    [self.secondSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect), CGRectGetMaxY(myRect))];
+    [self.secondSidePath addLineToPoint:CGPointMake(CGRectGetMinX(myRect)+LineLength, CGRectGetMaxY(myRect))];
+    self.secondSideLayer.path = self.secondSidePath.CGPath;
+    self.secondSideLayer.lineWidth = LineWeight;
+    self.secondSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+    //右下角
+    self.thirdSidePath = [UIBezierPath bezierPath];
+    [self.thirdSidePath moveToPoint:CGPointMake(CGRectGetMaxX(myRect)-LineLength, CGRectGetMaxY(myRect))];
+    [self.thirdSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMaxY(myRect))];
+    [self.thirdSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMaxY(myRect)-LineLength)];
+    self.thirdSideLayer.path = self.thirdSidePath.CGPath;
+    self.thirdSideLayer.lineWidth = LineWeight;
+    self.thirdSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+    //右上角
+    self.fourthSidePath = [UIBezierPath bezierPath];
+    [self.fourthSidePath moveToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMinY(myRect)+LineLength)];
+    [self.fourthSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect), CGRectGetMinY(myRect))];
+    [self.fourthSidePath addLineToPoint:CGPointMake(CGRectGetMaxX(myRect)-LineLength, CGRectGetMinY(myRect))];
+    self.fourthSidePath.lineCapStyle = kCGLineCapRound;
+    self.fourthSidePath.lineJoinStyle = kCGLineJoinRound;
+    self.fourthSideLayer.path = self.fourthSidePath.CGPath;
+    self.fourthSideLayer.lineWidth = LineWeight;
+    self.fourthSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+    
+    _firstSideLayer.fillColor = [UIColor clearColor].CGColor;
+    _secondSideLayer.fillColor = [UIColor clearColor].CGColor;
+    _thirdSideLayer.fillColor = [UIColor clearColor].CGColor;
+    _fourthSideLayer.fillColor = [UIColor clearColor].CGColor;
+    
     self.shapeLayer.path = path.CGPath;
     self.shapeLayer.fillRule = kCAFillRuleEvenOdd;
     self.shapeLayer.fillColor = [UIColor blackColor].CGColor;
@@ -115,19 +228,31 @@
 - (void) drawRect:(CGRect)rect {
     
     if (_isClearnBorder) {
-        _circlePath.lineWidth = 1;
+        _circlePath.lineWidth = 2;
         UIColor *color = [UIColor clearColor];
         [color set];
         _circlePath.lineCapStyle = kCGLineCapRound;
         _circlePath.lineJoinStyle = kCGLineJoinRound;
         [_circlePath stroke];
+        
+        _firstSideLayer.strokeColor = [UIColor clearColor].CGColor;
+        _secondSideLayer.strokeColor = [UIColor clearColor].CGColor;
+        _thirdSideLayer.strokeColor = [UIColor clearColor].CGColor;
+        _fourthSideLayer.strokeColor = [UIColor clearColor].CGColor;
+        
     }else{
         _circlePath.lineWidth = 2;
-        UIColor *color = [UIColor whiteColor];
+        UIColor *color = [UIColor clearColor];
         [color set];
         _circlePath.lineCapStyle = kCGLineCapRound;
         _circlePath.lineJoinStyle = kCGLineJoinRound;
         [_circlePath stroke];
+        
+        _firstSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        _secondSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        _thirdSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        _fourthSideLayer.strokeColor = [UIColor whiteColor].CGColor;
+        
     }
     
     
